a4641dbae42274a031c085baed88300db1e0cc95
inge <inge@inge-K70AB.(none)>
2012-04-10 19:54:49 +0200

    first commit

---
+++ dataSet.pl
@@ -0,0 +1,18 @@
+% Kennis Systemen Opdracht 1.2 Dataset
+% alle attributen en relaties van dieren worden hier beschreven
+%
+
+isa(animal, thing).
+isa(mammal, animal).
+isa(amfibian, animal).
+isa(reptile, animal).
+isa(fish, animal).
+isa(cow, mammal).
+isa(platypus, mammal).
+isa(dolphin, mammal).
+isa(african_green_tree_frog, amfibian ).
+isa(turtle, reptile).
+isa(boomslang, reptile).
+isa(stingray, fish).
+attribute(animal, living).
+attribute(mammal, legs, (2/4)).
