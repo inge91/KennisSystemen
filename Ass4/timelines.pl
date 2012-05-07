5b3cf6a1dbafa9b80cc1fc6101a13694a67aae57
Inge <inge.becht91@gmail.com>
2012-05-07 14:59:15 +0200

    wut

--- Ass4/timelines.pl
+++ Ass4/timelines.pl
@@ -1,13 +1,6 @@
-:-dynamic voor/2.
+:-dynamic na/2.
 
-voor(A,C):-
-    voor(A,B),
-    voor(B,C).
-
-na(C, A):-
-    na(C, B),
-    na(B, C).
-
+% a voor b en b voor c dan weet je niet of a voor b is of b voor c is 
 
 % punt predicate parses incoming sentence
 % and derives other facts
@@ -16,8 +9,57 @@ punt:-
     read(Time_constraint),
     parse(Time_constraint).
 
+% parse the input to make it assertable
 parse([A, voor, B]):-
-    assert(voor(A,B)).
+    assert(na(B,A)),
+    write('asserted na('),
+    write(B),
+    write(','),
+    write(A),
+    writeln(')'),
+    na_check.
+
+parse([A, na, B]):-
+    assert(na(A,B)),
+    write('asserted na('),
+    write(A),
+    write(','),
+    write(B),
+    writeln(')'),
+    na_check.
+
+% findall the arguments of na_rules()
+na_check:-
+    findall(X/Y, na(X,Y), List),
+    check_all_na(List).
+
+
+% for all argument pairs of na rules
+% check if ther are implicit rules
+check_all_na([]).
+
+check_all_na([X/Y|List]):-
+    na_check_one(X,Y),
+    check_all_na(List).
+
+% the implicit rule detector
+na_check_one(A, B):-
+    na(B, Z),!,
+    assert(na(A,Z)),
+    write('asserted na('),
+    write(A),
+    write(','),
+    write(Z),
+    writeln(')').
+
+na_check_one(B, C):-
+    na(Z, B),!,
+    assert(na(Z,C)),
+    write('asserted na('),
+    write(Z),
+    write(','),
+    write(C),
+    write(')').
+
+
     
-
-
