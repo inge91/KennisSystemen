ad2944a7b44b33ce7b39a1bbec817b1a900b5e49
Inge <inge.becht91@gmail.com>
2012-05-14 17:55:03 +0200

    beginning of backprop

--- Ass5/inge.pl
+++ Ass5/inge.pl
@@ -20,3 +20,36 @@ component(m3, mult, c, e, z).
 component(a1, add, x, y, f).
 component(a2, add, y, z, g).
 
+
+
+% backpropagation
+backprop:-
+    findall(X/Y, output(X,Y), Output),
+    backprop(Output).
+
+backprop([], []).
+
+% in case the predicted output is the same as the
+% measured output, go forth with measured output
+backprop([X/Y|T], ErrorSet):-
+    predictedValue(X, Y),
+    backprop(T, ErrorSet).
+
+backprop([X/Y|T], ErrorSet):-
+    % output X is not as predicted
+    \+predictedValue(X, Y),
+    % everything before X is put in the errorset
+    create_errorset(X, ErrorSet),
+    findall(F, component(_, _, F, F, X) , PreList).
+
+% combine values with a slash 
+combine_values([], [], []).
+
+combine_values([H|L1],)[H2|L2], [H1/H2|L3]):-
+    combine_values(L1, L2, L3).
+
+% create errorset creates an errorset
+% for the given element
+create_errorset(F, [L|ErrorSet] ):-
+    findall_values(F, L)
+    findall(X, component())
